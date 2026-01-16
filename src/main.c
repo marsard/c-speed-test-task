0831372ab029faddf793b65f49619a8c1dab3b01
marsard <martynas.skurdelis@gmail.com>
2026-01-16 13:28:56 +0200

    Reading function progress

--- src/main.c
+++ src/main.c
@@ -4,8 +4,33 @@
 #include <unistd.h>
 #include "cJSON.h"
 
-cJSON* read_json_file(const char* filename) {
-    return NULL;
+cJSON *read_json_file(const char *filename) {
+    FILE *stream = fopen(filename, "r");
+    if (!stream) {
+        fprintf(stderr, "error opening file: %s", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Get file size */
+    fseek(stream, 0, SEEK_END);
+    long size = ftell(stream);
+    fseek(stream, 0, SEEK_SET);
+
+    /* Read into buffer */
+    char *buffer = malloc(size + 1);
+    fread(buffer, 1, size, stream);
+    buffer[size] = '\0';
+    fclose(stream);
+
+    cJSON *json = cJSON_Parse(buffer);
+    if (!json) {
+        fprintf(stderr, "Parse error: %s\n", cJSON_GetErrorPtr());
+        free(buffer);
+        exit(EXIT_FAILURE);
+    }
+    free(buffer);
+
+    return json;
 }
 
 int main(int argc, char *argv[]) {
